6917f0a4d82746a1cf42a3529a26b8931281f0b2
ParkDongHyuck <donghyuckpark@ParkDongHyuckui-MacBook-Pro.local>
2017-03-30 17:46:36 +0900

    for문 문제 수정

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,12 +2,16 @@
 
 import UIKit
 
-let array = [1, 3, 5, 4, 2]
 
-array.sorted(by:{ (a: Int, b) -> Bool in
-    return a < b })
+// 문제 1. 1부터 순서대로 10개의 홀수를 출력하세요.
 
-//별찎기
+func makeOddNumArray () {
+    for i in 0...10 {
+        print(i * 2 + 1)
+    }
+}
+
+// 문제 2. *의 개수가 하나씩 증가하는 string 10개를 배열에 넣으세요. (*, **, ..., **********)
 
 var a: [Int] = [1,2,3,4,5]
 
@@ -27,38 +31,49 @@ var bb: String = star()
 
 bb
 
-// 3번
+// 문제 3. 13부터 숫자 7개를 더하세요.
 
 var sum:Int = 0
 for a in 13...20 {
     sum += a
 }
 
-// 5번 100 이하의 소수로 이루어진 배열을 만드시오.
-var result: [Int] = []
-var chk: Bool = true
 
+// 문제 4. 구구단 12단을 출력하세요. (12*9까지, 답만)
 
-for num in 2...100 {
-    for a in 2...num{
-        if(num == a) {
-            break
-        }
-        if num % a != 0 {
-            chk = true
-        }
-        else{
-            chk = false
-            break
-        }
-    }
-    if chk == true {
-        result += [num]
-        chk = false
+func gugu () {
+    for i in 1...9 {
+        print(i * 12)
     }
 }
 
-result
+// 100 이하의 소수를 모두 출력하세요.
 
-
+func primeNum () {
+    var primeNumbers : [Int] = []
+    
+    for i in 1...100 {
+        if (i == 1) {
+            continue
+        }
+            
+        else if (i == 2) {
+            primeNumbers += [2]
+        }
+            
+        else {
+            for j in 2...(i - 1) {
+                if (i % j == 0) {
+                    break;
+                }
+                
+                if (j == (i - 1)) {
+                    primeNumbers += [i]
+                }
+            }
+        }
+    }
+    
+    print(primeNumbers)
+}
 
